051f238777e6f72659e1bfbb827b0ad8b974b893
lexpynno <leo.pynnonen@gmail.com>
2015-03-22 20:46:40 +0200

    Viikko2

---
+++ PynnönenLeo/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,14 @@
+package ohtu.verkkokauppa;
+
+public class Viitegeneraattori implements ViitegeneraattoriInterface{
+    
+    private int seuraava;
+    
+    private Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    public int uusi(){
+        return seuraava++;
+    }
+}
